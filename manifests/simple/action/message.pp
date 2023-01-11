89e20ec5b15a0e4474ce9546ced6cbc53c43c09b
Romain Tartière <romain@blogreen.org>
2023-01-10 16:57:17 -1000

    Add data types

--- manifests/simple/action/message.pp
+++ manifests/simple/action/message.pp
@@ -1,10 +1,7 @@
 # default values are being ignored for now
 define patterndb::simple::action::message (
-  $values = {},
-  $tags = [],
-  $inherit_properties = false,
+  Hash $values = {},
+  Array $tags = [],
+  Boolean $inherit_properties = false,
 ) {
-  validate_bool($inherit_properties)
-  validate_hash($values)
-  validate_array($tags)
 }
